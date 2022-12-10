fba0b5fb86763017717364e50dfba54fc5298a80
X-dante-X <solomamd17@gmail.com>
2022-12-10 13:27:26 +0100

    Initial commit

---
+++ connect/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace connect.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public UsersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select user_id, user_login, user_password, user_email, user_register_date from users; ";
+
+            DataTable table= new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("Database");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query,myCon))
+                { 
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+       
+    }
+}
---
+++ connect/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace connect.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserLogin { get; set; }
+        public string UserPassword { get; set; }
+        public string UserEmail { get; set; }
+        public string UserRegisterDate { get; set; }
+    }
+}
